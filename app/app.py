3b2b36565086c9b981198405cadae7aabe6d8623
cynthiawuor <cynthiawuor254@gmail.com>
2023-10-31 12:05:27 +0300

    migrations

--- app/app.py
+++ app/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
 from models import db, User, Contact, Organization
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacthub.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+migrate = Migrate(app, db)
 db.init_app(app)
 
 with app.app_context():
