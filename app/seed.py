383b3164ab0fbd98b3d73da09dbe72a822e73cc2
WanjikuN <cikunjoroge4@gmail.com>
2023-11-01 16:59:23 +0300

    add seed data

--- app/seed.py
+++ app/seed.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from models import db, User, Contact, Organization
+from faker import Faker
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacthub.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
+
+fake = Faker()
 
 with app.app_context():
     db.create_all()
@@ -19,66 +22,47 @@ with app.app_context():
         # Commit the changes to the database
         db.session.commit()
 
+    def generate_valid_phone_number():
+        phone_number = fake.random_int(min=100000000, max=999999999)
+        return str(phone_number)
+
     def seed_data():
         # Clear tables before seeding
         clear_tables()
 
         # Create sample users
-        user1 = User(
-            username="john_doe",
-            gender="Male",
-            phone_number="123456789",
-            email="john@example.com",
-            password="Password123",
-            address="123 Main St"
-        )
+        for _ in range(20):  # Change 2 to the number of initial users you want
+            user = User(
+                username=fake.user_name(),
+                gender=fake.random_element(elements=('Male', 'Female')),
+                phone_number=generate_valid_phone_number(),
+                email=fake.email(),
+                password=fake.password(),
+                address=fake.address()
+            )
+            db.session.add(user)
 
-        user2 = User(
-            username="jane_doe",
-            gender="Female",
-            phone_number="987654321",
-            email="jane@example.com",
-            password="SecurePasswo1rd",
-            address="456 Oak St"
-        )
-
-        # Add users to the session
-        db.session.add(user1)
-        db.session.add(user2)
         db.session.commit()
+
         # Create sample organizations
-        org1 = Organization(
-            name="TechCorp",
-            email="info@techcorp.com",
-            address="789 Tech St"
-        )
+        for _ in range(20):  # Change 2 to the number of initial organizations you want
+            org = Organization(
+                name=fake.company(),
+                email=fake.company_email(),
+                address=fake.address()
+            )
+            db.session.add(org)
 
-        org2 = Organization(
-            name="Foodies Inc.",
-            email="info@foodiesinc.com",
-            address="321 Food St"
-        )
-
-        # Add organizations to the session
-        db.session.add(org1)
-        db.session.add(org2)
         db.session.commit()
+
         # Create sample contacts
-        contact1 = Contact(
-            profile_notes="Met at a conference",
-            user_id=user1.id,
-            organization_id=org1.id
-        )
-
-        contact2 = Contact(
-            profile_notes="Networking event",
-            user_id=user2.id,
-            organization_id=org2.id
-        )
-
-        # Add contacts to the session
-        db.session.add(contact1)
-        db.session.add(contact2)
+        for _ in range(20):  # Change 2 to the number of initial contacts you want
+            contact = Contact(
+                profile_notes=fake.sentence(),
+                user_id=fake.random_element(elements=User.query.with_entities(User.id).all())[0],
+                organization_id=fake.random_element(elements=Organization.query.with_entities(Organization.id).all())[0]
+            )
+            db.session.add(contact)
 
         # Commit the changes to the database
         db.session.commit()
