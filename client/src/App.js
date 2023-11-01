f09d697c64b51d400f90d42c2161d88f8a90e1ea
WanjikuN <cikunjoroge4@gmail.com>
2023-11-01 14:08:33 +0300

    feat(Routes): add contacts routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
 import logo from "./logo.svg";
 import React from "react";
-//import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom'; 
 import "./App.css";
 import NavbarComp from "./Components/Navbar";
-
+import Contacts from "./Components/Contacts";
 function App() {
   return (
     <div className="App">
       <NavbarComp />
-      <h2>Contact Hub</h2>
-      {/* <Routes>
-        <Route path="/" />
-      </Routes> */}
+      <Routes>
+          <Route exact path='/contacts' element={<Contacts />} />
+      </Routes>
+      
     </div>
   );
 }
