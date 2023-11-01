cc20d217164b73d28bf39a28fc042730e1caf2d0
WanjikuN <cikunjoroge4@gmail.com>
2023-11-01 17:01:43 +0300

    feat: add contacts component

--- client/src/Components/Contacts.js
+++ client/src/Components/Contacts.js
@@ -1,7 +1,26 @@
+import {useState, useEffect} from "react"
+import Contact from "./Contact"
 export default function Contacts(){
+    const [contacts, setContacts] = useState([])
+    useEffect(()=>{
+        fetch('/contacts')
+        .then(res => res.json())
+        .then(data => setContacts(data))
+    },[])
     return (
-        <>
-        <h1>Contacts</h1>
-        </>
+        <div id ='listy'>
+        
+        {contacts.map(contact => {
+            return( <Contact key={contact.id} id={contact.id} 
+                username={contact.user.username} 
+                email={contact.user.email} 
+                gender={contact.user.gender}
+                phone={contact.user.phone_number}
+                address={contact.user.address}
+                org={contact.organization.name}
+                notes={contact.profile_notes}
+                />
+        )})}
+        </div>
     )
 }
