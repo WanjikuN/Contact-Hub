8de76827601855e522d20cfbe0ad426dfb5af29d
WanjikuN <cikunjoroge4@gmail.com>
2023-11-01 14:15:23 +0300

    feat(Routes): add organizations routing

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,15 @@ import { Routes, Route, Link } from 'react-router-dom';
 import "./App.css";
 import NavbarComp from "./Components/Navbar";
 import Contacts from "./Components/Contacts";
+import Organizations from "./Components/Organizations";
 function App() {
   return (
     <div className="App">
       <NavbarComp />
       <Routes>
           <Route exact path='/contacts' element={<Contacts />} />
+          <Route exact path='/organizations' element={<Organizations />} />
+
       </Routes>
       
     </div>
