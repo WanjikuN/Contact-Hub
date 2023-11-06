5628de4a1319441f4d16512c6247105050b27501
WanjikuN <cikunjoroge4@gmail.com>
2023-11-06 17:01:21 +0300

    update proxy configurations

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import os
 
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}}) 
+CORS(app, resources={r"/*": {"origins": "https://contacthub-client.onrender.com"}}) 
 
 # app.secret_key = "qwertyyuiop"
 # app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
